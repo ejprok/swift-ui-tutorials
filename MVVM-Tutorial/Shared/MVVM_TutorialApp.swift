63a4a81e59d783d5731d3d0494848f49d6259d17
Edward Prokopik <ejprok@gmail.com>
2022-07-07 19:06:47 -0700

    Add empty mvvm project

---
+++ MVVM-Tutorial/Shared/MVVM_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_TutorialApp.swift
+//  Shared
+//
+//  Created by EJ Prokopik on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
